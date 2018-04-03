209c87df906403372b7cbe72d1ae63fa719a23ff
miso999 <michal.simoncic@gmail.com>
2018-04-03 10:25:51 +0200

    categoies

--- functions.php
+++ functions.php
@@ -274,3 +274,16 @@ function rezervacie()
 
     echo "</table>";
 }
+
+add_filter('pre_get_posts', 'query_post_type');
+function query_post_type($query) {
+    if( is_category() ) {
+        $post_type = get_query_var('post_type');
+        if($post_type)
+            $post_type = $post_type;
+        else
+            $post_type = array('product', 'post'); // don't forget nav_menu_item to allow menus to work!
+        $query->set('post_type',$post_type);
+        return $query;
+    }
+}
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,26 +10,26 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+    <header class="entry-header">
+        <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-				futbalove_travniky_posted_on();
-				futbalove_travniky_posted_by();
-			?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+        <?php if ( 'post' === get_post_type() ) : ?>
+            <div class="entry-meta">
+                <?php
+                futbalove_travniky_posted_on();
+                futbalove_travniky_posted_by();
+                ?>
+            </div><!-- .entry-meta -->
+        <?php endif; ?>
+    </header><!-- .entry-header -->
 
-	<?php futbalove_travniky_post_thumbnail(); ?>
+    <?php futbalove_travniky_post_thumbnail(); ?>
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+    <div class="entry-summary">
+        <?php the_excerpt(); ?>
+    </div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
-		<?php futbalove_travniky_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+    <footer class="entry-footer">
+        <?php futbalove_travniky_entry_footer(); ?>
+    </footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
